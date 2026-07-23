517f4c9e10a1966ec08f4a3246e7291e2804cc7c
solo243 <welcomesir48u@gmail.com>
2026-07-24 00:47:22 +0530

    init

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -111,6 +112,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main className="grow px-4 w-full ">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
